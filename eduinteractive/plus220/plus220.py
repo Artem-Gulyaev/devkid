011762624fac12aaf4a1379ecdd8c372d207cc06
Artem Gulyaev <gulyaev.artem.a@gmail.com>
2021-06-02 16:38:10 +0200

    [polishing] Answer check looks okay

--- eduinteractive/plus220/plus220.py
+++ eduinteractive/plus220/plus220.py
@@ -13,6 +13,14 @@ from kivy.core.window import Window
 
 #Window.size = (1900, 1100)
 
+first_arg_color = [1.0, 0.5, 0.3, 1]
+second_arg_color = [0.3, 1, 0.3, 1]
+
+neutral_color = [1, 1, 1, 1]
+
+pass_color = [0, 1, 0, 1]
+fail_color = [1, 0, 0, 1]
+
 class TestApp(App):
 
     def __init__(self):
@@ -24,14 +32,14 @@ class TestApp(App):
         if not hasattr(a, "mode"):
             a.mode = 'white'
         if a.mode == 'green':
-            a.background_color = [1, 0, 0, 1]
-            a.mode = 'red'
-        elif a.mode == 'red':
-            a.background_color = [1, 1, 1, 1]
+            a.background_color = neutral_color
             a.mode = 'white'
-        elif a.mode == 'white':
-            a.background_color = [0, 1, 0, 1]
+        elif a.mode == 'red':
+            a.background_color = second_arg_color
             a.mode = 'green'
+        elif a.mode == 'white':
+            a.background_color = first_arg_color
+            a.mode = 'red'
 
     def check_answer(self, target):
         a = 0
@@ -82,25 +90,31 @@ class TestApp(App):
             if self.answer1.text != "" and self.answer1.text != "0":
                 print("FAILED! At first digit of the answer. Expected:"
                       " '0' or empty, actual: '%s'" % (self.answer1.text))
-                self.answer1.background_color = [1.0, 0.2, 0.2, 1.0]
+                self.answer1.background_color = fail_color
                 result = False
+            else:
+                self.answer1.background_color = pass_color
         else:
             if self.answer1.text != str((a + b) // 10):
                 print("FAILED! At first digit of the answer. Expected:"
                       " '%d' , actual: '%s'" % ((a + b) // 10, self.answer1.text))
-                self.answer1.background_color = [1.0, 0.2, 0.2, 1.0]
+                self.answer1.background_color = fail_color
                 result = False
+            else:
+                self.answer1.background_color = pass_color
 
         if self.answer2.text != str((a + b) % 10):
             print("FAILED! At second digit of the answer. Expected:"
                   " '%d' , actual: '%s'" % ((a + b) % 10, self.answer2.text))
-            self.answer2.background_color = [1.0, 0.2, 0.2, 1.0]
+            self.answer2.background_color = fail_color
             result = False
+        else:
+            self.answer2.background_color = pass_color
         
         if result:
-            self.check_button.background_color = [0.3, 0.3, 1.0, 1.0]
+            self.check_button.background_color = pass_color
         else:
-            self.check_button.background_color = [1.0, 0.2, 0.2, 1.0]
+            self.check_button.background_color = fail_color
 
         return result
 
@@ -132,14 +146,14 @@ class TestApp(App):
                     text='First\nnumber',
                     values=('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
                     , font_size=self.font_size
-                    , color=[1,0,0,1])
+                    , color=first_arg_color)
         args.add_widget(self.arg1)
         args.add_widget(Label(text='+', font_size=self.font_size))
         self.arg2 = Spinner(
                     text='Second\nnumber'
                     , values=('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
                     , font_size=self.font_size
-                    , color=[0,1,0,1])
+                    , color=second_arg_color)
         args.add_widget(self.arg2)
 
         main_layout.add_widget(args)
@@ -167,7 +181,7 @@ class TestApp(App):
         self.check_button = Button(text='', on_press=self.check_answer)
 
         main_layout.add_widget(Label(text='And check the result!'
-                                     , font_size=self.font_size, color=[1,0,0,1]))
+                                     , font_size=self.font_size, color=neutral_color))
         main_layout.add_widget(self.check_button)
 
         main_layout.size_hint = (1.0, 1.0)
